bed2115449e501bad1ad5832bd13bc1206a930cf
Unmesh Bari <unmeshbari5@gmail.com>
2025-06-02 00:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+
+ const delhiRadio = document.getElementById("delhi");
+  const blockquote = document.getElementById("info");
+
+  // Add event listener
+  document.querySelectorAll("input[name='options']").forEach(radio => {
+    radio.addEventListener("change", () => {
+      if (delhiRadio.checked) {
+        blockquote.style.display = "block";
+      } else {
+        blockquote.style.display = "none";
+      }
+    });
+  });
